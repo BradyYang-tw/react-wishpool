241af5d134cf73dd1bc927c0b8f56a8dc5dca51c
HsinYu <hsinyu.yang@hwacom>
2023-01-03 23:17:15 +0800

    建立許願頁面

--- src/pages/WishPage.jsx
+++ src/pages/WishPage.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Layout from "../UI/Layout";
+import "./WishPage.css";
 
 const WishPage = () => {
   return (
     <Layout>
-      <div class="wishpage">Make a Wish</div>;
+      <div className="wishpage-title">我要許願</div>
+      <form>
+        <div className="wishpage-formitem">
+          <label htmlFor="wishpage-description">描述一直占用你時間的事情</label>
+          <textarea id="wishpage-description"></textarea>
+        </div>
+      </form>
     </Layout>
   );
 };
